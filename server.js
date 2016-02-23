dea0f0daae3c288eb3664418edd1896b3719e75b
chriscampbell32 <chriscampbell32@yahoo.com>
2016-02-23 14:55:20 -0500

    registration working and adding to database

--- server.js
+++ server.js
@@ -7,27 +7,31 @@ var bcrypt = require('bcryptjs');
 
 var app = express();
 var PORT = process.env.NODE_ENV || 8090;
-
+//sequelize init
 var Sequelize = require('sequelize');
+//connect to database
 var sequelize = new Sequelize('student_teacher_db', 'root');
 
+//models
 var Students = sequelize.define('Students', {
     id: {
-        type: Sequelize.STRING,
+        type: Sequelize.INTEGER,
         autoIncrement: true,
         primaryKey: true
     },
     firstname: Sequelize.STRING,
     lastname: Sequelize.STRING,
-    email: Sequelize.STRING
+    email: Sequelize.STRING,
+    password: Sequelize.STRING
 });
-
+//instructor model...
+//body parser reads info from html
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-//sequelize
 
+//make main.handlebars default layout
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
@@ -39,8 +43,14 @@ app.get('/login', function(req, res){
 });
 
 app.get('/registration', function(req,res){
-    res.render('login', {
-     //   msg: req.query.msg
+    res.render('registration');
+});
+
+app.post('/registration', function(req, res){
+    Students.create(req.body).then(function(user){
+        res.redirect('/secret');
+    }).catch(function(err){
+        res.send("/registration?msg=erroromgomg");
     });
 });
 
@@ -48,13 +58,23 @@ app.get('/students', function(req, res){
     res.send('students');
 });
 
+
+
 app.get('/instructors', function(req, res){
     res.send('instructors');
+});
+
+app.get('/secret', function(req, res){
+    Students.findAll().then(function(user){
+        res.render('secret', {user});
+    });
 });
 
 
 
 
-app.listen(PORT, function(){
-    console.log("Listening on PORT %s", PORT);
+sequelize.sync().then(function(){
+    app.listen(PORT, function(){
+        console.log("listening on port %s", PORT);
+    });
 });
